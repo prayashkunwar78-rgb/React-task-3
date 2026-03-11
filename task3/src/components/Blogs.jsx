43828bdbc2fa453e550cc467e06a58aa38ca3d7d
prayashkunwar78-rgb <prayashkunwar78@gmail.com>
2026-03-11 22:58:55 +0545

    [ADD]:learning-blogs

--- task3/src/components/Blogs.jsx
+++ task3/src/components/Blogs.jsx
@@ -4,7 +4,7 @@ function Blogs() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts?_limit=5")
+    fetch("https://jsonplaceholder.typicode.com/posts")
       .then((res) => res.json())
       .then((data) => setPosts(data));
   }, []);
